da110765e4535624dab98b54c4d0437d64f829be
Sanghee Kim <sangheestyle@gmail.com>
2017-12-10 15:10:42 -0500

    subscription geoPositionAdded: GeoPosition!
    Will explain later!

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { makeExecutableSchema } = require('graphql-tools')
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { execute, subscribe } = require('graphql')
+const { PubSub, withFilter } = require('graphql-subscriptions')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+const { createServer } = require('http')
 
 // Step 1: Create schema and sample data
 const typeDefs = `
@@ -18,11 +22,16 @@ type Mutation {
   addGeoPosition(latitude: Float!, longitude: Float!): GeoPosition!
 }
 
+type Subscription {
+  geoPositionAdded: GeoPosition!
+}
+
 # we need to tell the server which types represent the root query
 # and root mutation types. We call them RootQuery and RootMutation by convention.
 schema {
   query: Query
   mutation: Mutation
+  subscription: Subscription
 }
 `
 
@@ -34,6 +43,8 @@ const geoPositions = [
   { latitude: 40.7847102, longitude: 73.977418 }
 ]
 
+const pubsub = new PubSub();
+
 const resolvers = {
   Query: {
     geoPositions: () => {
@@ -44,7 +55,15 @@ const resolvers = {
     addGeoPosition: (object, variables, context, resolveInfo) => {
       const { latitude, longitude } = variables;
       geoPositions.push({ latitude, longitude });
+      pubsub.publish('geoPositionAdded', { geoPositionAdded: { latitude, longitude } });
       return { latitude, longitude };
+    }
+  },
+  Subscription: {
+    geoPositionAdded: {
+      subscribe: withFilter(() => pubsub.asyncIterator('geoPositionAdded'), (payload, variables) => {
+        return true;
+      }),
     }
   },
   GeoPosition: {
@@ -64,7 +83,22 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.get('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+  subscriptionsEndpoint: 'ws://localhost:3000/subscriptions',
+}));
 
-// Step 3: Make express server listen requests on port 3000
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const server = createServer(app);
+
+server.listen(3000, () => {
+  console.log('GraphQL server is now running on port 3000');
+
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema,
+  }, {
+    server,
+    path: '/subscriptions',
+  });
+});
